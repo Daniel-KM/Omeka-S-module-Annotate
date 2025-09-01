82d17c52c48ed0bea06b116d786a3ad71f3a2eb7
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-09-01 00:00:00 +0000

    Released version 3.4.11.

--- Module.php
+++ Module.php
@@ -53,7 +53,7 @@ use Omeka\Module\AbstractModule;
 /**
  * Annotate
  *
- * @copyright Daniel Berthereau, 2017-2024
+ * @copyright Daniel Berthereau, 2017-2025
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  */
 class Module extends AbstractModule
